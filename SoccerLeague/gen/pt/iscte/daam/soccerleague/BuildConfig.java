588e7efb7ee23fab6664e142ed5ef4e802b63879
Carlos Serrao <me@carlosserrao.net>
2014-03-25 00:21:15 +0000

    Changes in structure

---
+++ SoccerLeague/gen/pt/iscte/daam/soccerleague/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pt.iscte.daam.soccerleague;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ WebviewSample/gen/pt/iscte/daam/webviewsample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pt.iscte.daam.webviewsample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
